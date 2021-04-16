da17b70876eb0c1eabc3cdffd9846ef4f2ce2857
Tanner Swett <tannerswett@gmail.com>
2021-04-16 19:47:43 -0400

    Add char_encoding parameter to create_char_rnn.py

--- src/create_char_rnn.py
+++ src/create_char_rnn.py
@@ -9,6 +9,8 @@ parser = argparse.ArgumentParser(
     description="Create a CharRnn model (along with a trainer).")
 
 parser.add_argument('--dataset', help="A text file to use as the dataset")
+parser.add_argument('--char_encoding', default='utf-8', help="The character encoding to use when reading from the file")
+parser.add_argument('--char_errors', default='replace', help="The error behavior when encountering unrecognizable characters in the file")
 parser.add_argument('--hidden_size', type=int, default=300)
 parser.add_argument('--num_layers', type=int, default=3)
 parser.add_argument('--chunk_size', type=int, default=200)
@@ -17,15 +19,21 @@ parser.add_argument('--model_name', help="The path to store the model at")
 
 args = parser.parse_args()
 
-document = open(args.dataset, errors='replace').read()
+print("Reading document...")
+document = open(args.dataset, encoding=args.char_encoding, errors=args.char_errors).read()
 document_alphabet = ''.join(set(document))
 
+print("Creating model...")
 document_model = CharRnn(
     document_alphabet, hidden_size=args.hidden_size, num_layers=args.num_layers)
 
+print("Creating trainer...")
 document_trainer = CharRnnTrainer(
     document_model, document, chunk_size=args.chunk_size,
     batch_size=args.batch_size)
 
+print("Writing trainer to file...")
 output_file = open(args.model_name, mode='wb')
 pickle.dump(document_trainer, output_file)
+
+print("Done.")
